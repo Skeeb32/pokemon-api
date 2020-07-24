313bfd6539ca1761be64e2fb540ce466eedf001b
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-07-24 01:06:02 -0700

    Pokemon

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {getAllPokemon, getPokemon} from './services/pokemon'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      let pokemon = await loadingPokemon(response.results);
+      
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  const loadingPokemon = async (data) => {
+    let _pokemon = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon);
+      return pokemonRecord
+    }))
+  }
+
+  setPokemonData(_pokemonData)
+
+  return <div>{loading ? <h1>Loding...</h1> : <h1>Data is fetched</h1>}</div>;
 }
 
 export default App;
