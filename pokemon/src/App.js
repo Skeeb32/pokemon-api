bbc8d1856c0371c5bdadad49806b00caa9b9b783
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-07-26 03:32:02 -0700

    Pokemon.js

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -36,22 +36,31 @@
 // export default App;
 
 import React, { Component } from 'react';
+import {HashRouter as Router, ROute, Switch, Route } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import DashBoard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
+
+import backgroundImage from './pattern.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router> 
+      <div className="App" style={{ background: `url(${backgroundImage})`}}>
         <NavBar />
         <div className="container">
-          <DashBoard />
+          <Switch> 
+            <Route exact path="/" component={DashBoard} />
+            <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+          </Switch>
         </div>
       </div>
+      </Router>
     )
   }
 }
