fc2d1f5b9dbc88d6fa14e8a7a20f49fdf0148cd4
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-07-26 23:30:04 -0700

    Name

--- pokemon/src/components/pokemon/Pokemon.js
+++ pokemon/src/components/pokemon/Pokemon.js
@@ -3,10 +3,35 @@ import axios from 'axios'
 
 
 export default class Pokemon extends Component {
+
+    state = {
+        name: '',
+        pokemonIndex: '',
+        imageUrl: '',
+        // name: '',
+    }
+
+    async componentDidMount() {
+        const { pokemonIndex } = this.props.match.params;
+
+
+    // URL for pokemon Information
+    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`
+    const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`
+
+    // Get Pokemon Information
+    const pokemonRes = await axios.get(pokemonUrl);   
+    
+    const name = pokemonRes.data.name;
+    this.setState({ name })
+}
+
+
+
     render() {
         return (
             <div>
-                
+                <h1>{this.state.name}</h1>
             </div>
         )
     }
